0b5a58268921c76e23609146d0da4463d0b2046d
Yama <yamawaki00@gmail.com>
2019-04-13 03:20:20 +0900

    modify: gulp version update and fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const gulpif = require('gulp-if');
 const minimist = require('minimist');
 const del = require('del');
 const browserSync = require('browser-sync').create();
-const runSequence = require('run-sequence');
+const runSequence = require('gulp4-run-sequence');
 
 const webpackConfig = require('./webpack.config');
 
@@ -27,88 +27,95 @@ const options = minimist(process.argv.slice(2), config);
 let isProd = (options.env === 'prod') ? true : false;
 console.log('[build env]', options.env, '[isProd]', isProd);
 
-gulp.task('browser-sync', function(){
-	browserSync.init({
-		server: {
-			baseDir: destDir
-		}
-	});
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: destDir
+    }
+  });
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   return gulp.src(['src/pc/styles/**/*.scss'])
-    .pipe(plumber({
-      errorHandler: notify.onError('Error: <%= error.message %>')
-    }))
-    .pipe(sass( {
-      outputStyle: 'expanded'
-    }))
-		.pipe(rename(function (path) {
-			path.dirname = 'css'
-		}))
-		.pipe(autoprefixer({
-      browsers: ['last 2 version', 'iOS >= 9', 'Android >= 4.6'],
-      cascade: false
-    }))
-		.pipe(gulpif(isProd, cleanCss()))
-    .pipe(gulpif(!isProd, gulp.dest(destDir)))
-		.pipe(gulpif(isProd, gulp.dest(prodDir)))
+  .pipe(plumber({
+    errorHandler: notify.onError('Error: <%= error.message %>')
+  }))
+  .pipe(sass( {
+    outputStyle: 'expanded'
+  }))
+  .pipe(rename(function (path) {
+    path.dirname = 'css'
+  }))
+  .pipe(autoprefixer({
+    browsers: ['last 2 version', 'iOS >= 9', 'Android >= 4.6'],
+    cascade: false
+  }))
+  .pipe(gulpif(isProd, cleanCss()))
+  .pipe(gulpif(!isProd, gulp.dest(destDir)))
+  .pipe(gulpif(isProd, gulp.dest(prodDir)))
 });
 
 
-gulp.task('html', function() {
-	return gulp.src(['src/pc/html/**/*.html'])
-	.pipe(gulpif(!isProd, gulp.dest(destDir + '/')))
-	.pipe(gulpif(isProd, gulp.dest(prodDir + '/')))
+gulp.task('html', () => {
+  return gulp.src(['src/pc/html/**/*.html'])
+  .pipe(gulpif(!isProd, gulp.dest(destDir + '/')))
+  .pipe(gulpif(isProd, gulp.dest(prodDir + '/')))
 });
 
 
-gulp.task('images', function() {
-	return gulp.src(['src/pc/images/**/'])
-	.pipe(gulpif(!isProd, gulp.dest(destDir + 'images/')))
-	.pipe(gulpif(isProd, gulp.dest(prodDir + 'images/')))
+gulp.task('images', () => {
+  return gulp.src(['src/pc/images/**/'])
+  .pipe(gulpif(!isProd, gulp.dest(destDir + 'images/')))
+  .pipe(gulpif(isProd, gulp.dest(prodDir + 'images/')))
 });
 
-gulp.task('webpack', function() {
-	return webpackStream(webpackConfig, webpack)
-	.pipe(gulpif(!isProd, gulp.dest(destDir + 'js/riot/')))
-	.pipe(gulpif(isProd, gulp.dest(prodDir + 'js/riot/')))
+gulp.task('webpack', () => {
+  return webpackStream(webpackConfig, webpack)
+  .on('error', function handleError() {
+  this.emit('end');
+  })
+  .pipe(gulpif(!isProd, gulp.dest(destDir)))
+  .pipe(gulpif(isProd, gulp.dest(prodDir)))
 });
 
 
-gulp.task('bs-reload', function(){
-	browserSync.reload();
+gulp.task('bs-reload', () => {
+  browserSync.reload();
 });
 
 gulp.task('clean', del.bind(null, prodDir));
 
-gulp.task('build', ['sass', 'images', 'webpack', 'html'], function() {
-});
+
+gulp.task('build', gulp.series(
+  gulp.parallel('sass', 'webpack', 'html', 'images')
+));
 
 
-gulp.task('default', ['browser-sync', 'sass', 'images', 'webpack', 'html'], function() {
-	watch(['src/pc/styles/**/*.scss'], function() {
-		return runSequence(
-			'sass',
-			'bs-reload'
-		);
-	});
-	watch(['src/pc/tags/**/*.tag', 'src/pc/js/app.js'], function() {
-		return runSequence(
-			'webpack',
-			'bs-reload'
-		)
-	});
-	watch(['src/pc/images/**/*'], function() {
-		return runSequence(
-			'images',
-			'bs-reload'
-		);
-	});
-	watch(['src/pc/html/**/*'], function() {
-		return runSequence(
-			'html',
-			'bs-reload'
-		);
-	});
-});
+gulp.task('default', gulp.series(
+  gulp.parallel('browser-sync', 'sass', 'webpack', 'html', 'images', () => {
+    watch(['src/pc/styles/**/*.scss'], () => {
+      return runSequence(
+        'sass',
+        'bs-reload'
+      );
+    });
+    watch(['src/pc/tags/**/*.tag', 'src/pc/js/app.js'], () => {
+      return runSequence(
+        'webpack',
+        'bs-reload'
+      );
+    });
+    watch(['src/pc/images/**/*'], () => {
+      return runSequence(
+        'images',
+        'bs-reload'
+      );
+    });
+    watch(['src/pc/html/**/*'], () => {
+      return runSequence(
+        'html',
+        'bs-reload'
+      );
+    });
+  })
+));
