a1abaa0151eaf1016529741f6cbb7cf0f0ba8f97
Yama <yamawaki00@gmail.com>
2021-05-21 21:31:52 +0900

    modify: app.js整理

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,6 @@ import 'regenerator-runtime/runtime';
 import '@webcomponents/template';
 
 import * as riot from 'riot';
-import hello from '../riot/hello.riot';
+import hello from '@/riot/hello';
 riot.register('hello', hello);
 riot.mount('hello');
-
-
-async function hoge() {
-  try {
-    const data = '!!!';
-    return data;
-  } catch(err) {
-    console.error(err);
-  }
-}
-
-hoge().then((data) => {
-  console.log(data);
-});
